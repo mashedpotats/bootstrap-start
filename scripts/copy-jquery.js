98592a35d70a194651259c2e712d0ba7262c8875
mashedpotats <samuelli4521@gmail.com>
2016-11-27 18:11:17 -0800

    Fixed bugs

--- scripts/copy-jquery.js
+++ scripts/copy-jquery.js
@@ -5,7 +5,7 @@
 const fs = require('fs-extra');
 
 const JQUERY_LOCATION = 'node_modules/jquery/dist/jquery.min.js';
-const JQUERY_DESTINATION = 'public/javascripts/jquery.min.js';
+const JQUERY_DESTINATION = 'public/js/jquery.min.js';
 
 console.log('Copying jQuery from' + JQUERY_LOCATION);
 fs.copy(JQUERY_LOCATION, JQUERY_DESTINATION, function(err) {
